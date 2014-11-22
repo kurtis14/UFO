e91766d765c99a0abe38a16e71e0d919b7d7fe6a
Kurtis Stewart <kurtisstewart1948@gmail.com>
2014-11-21 22:47:43 -0800

    edits to latest.sightings fn; deleted extra file

--- R/latest.sightings.r
+++ R/latest.sightings.r
@@ -1,8 +1,21 @@
-latest.sightings <- function(area = 'BC', return.rows = 5){
+#' Is it a bird? Is it a plane? It's a UFO!
+#' 
+#' This function returns the latest sightings of Unidentified Flying Objects 
+#' in the area (i.e., state, province, territory) specified. 
+#' @param area A two-letter term in uppercase that specifies the area to search
+#' @param return.rows The number of most recent sightings to return
+#' @keywords misc
+#' @export
+#' @examples
+#' latest.sightings()
+#' latest.sightings(area='BC')
+#' latest.sightings(area='NC',10)
+#' latest.sightings(area='MA',3)
+
+latest.sightings <- function(area = 'BC', return.rows = 5, ...){
 	url <- paste0('http://www.nuforc.org/webreports/ndxl', area, '.html')
 	table <- XML::readHTMLTable(url)
 	df <- data.frame(table)
 	names(df) <- c('date_and_time','city','state','shape','duration','summary','posted')
 	return(head(df,return.rows))
 }
-latest.sightings(area='BC')
