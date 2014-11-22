74cb98d29b88b1b740e2f865fb48b24da86dcff8
Kurtis Stewart <kurtisstewart1948@gmail.com>
2014-11-22 00:06:18 -0800

    added vignette and page for ?ufo

---
+++ R/UFO.R
@@ -0,0 +1,10 @@
+#' R package: UFO
+#' 
+#' Find the latest reported UFO sightings in different states, provinces and territories across Canda and the USA
+#' 
+#' The functions in the UFO package help you find the most recent sightings by area and
+#' the types (i.e., shapes) of UFO's reported
+#' 
+#' @docType package
+#' @name ufo
+NULL
--- R/latest.sightings.r
+++ R/latest.sightings.r
@@ -1,21 +1,33 @@
-#' Is it a bird? Is it a plane? It's a UFO!
+#' Find latest UFO sightings
 #' 
-#' This function returns the latest sightings of Unidentified Flying Objects 
+#' This function returns the most recent sightings of Unidentified Flying Objects 
 #' in the area (i.e., state, province, territory) specified. 
-#' @param area A two-letter term in uppercase that specifies the area to search
+#' @param area Two uppercase letters that specifies the area to search
 #' @param return.rows The number of most recent sightings to return
 #' @keywords misc
 #' @export
 #' @examples
 #' latest.sightings()
 #' latest.sightings(area='BC')
-#' latest.sightings(area='NC',10)
-#' latest.sightings(area='MA',3)
+#' latest.sightings(area='NC', return.rows = 10)
+#' latest.sightings(area='MA', 3)
 
-latest.sightings <- function(area = 'BC', return.rows = 5, ...){
+latest.sightings <- function(area = 'BC', return.rows = 5){
+	
 	url <- paste0('http://www.nuforc.org/webreports/ndxl', area, '.html')
 	table <- XML::readHTMLTable(url)
 	df <- data.frame(table)
-	names(df) <- c('date_and_time','city','state','shape','duration','summary','posted')
+	names(df) <- c('date.time','city','state','shape','duration','summary','posted')
+	
+	df$date.time <- as.character(df$date.time)
+	df[,8:9] <- data.frame(stringr::str_split_fixed(df$date.time, ' ', 2))
+	names(df) <- c('date.time','city','state','shape','duration','summary','posted','date','time')
+	
+	library(dplyr)
+	
+	df <- df %>%
+		select(date,time,posted,state,city,shape,duration,summary)
+	
 	return(head(df,return.rows))
 }
+
